e73ba5c6b1b5ccdbdb669d0c2192ec1dfce528a1
Pavel Mitin <pm@anahoret.com>
2008-12-15 16:56:35 +0200

    kernel#retryable was enchanced. :delay option was added. s3_store class uses delay option.
    Signed-off-by: Damien Tanner <damien@new-bamboo.co.uk>

--- lib/core_extensions/kernel.rb
+++ lib/core_extensions/kernel.rb
@@ -5,6 +5,7 @@ module Kernel
   #           Note this means it actually tries 2 times!
   # * :on -   The Exception on which a retry will be performed. Defaults to 
   #           Exception, which retries on any Exception.
+  # * :delay - Delay in seconds before the next try (optional)
   #
   # Example
   # =======
@@ -15,11 +16,12 @@ module Kernel
   def retryable(options = {}, &block)
     opts = { :tries => 1, :on => Exception }.merge(options)
 
-    retry_exception, retries = opts[:on], opts[:tries]
+    retry_exception, retries, delay = opts[:on], opts[:tries], opts[:delay]
 
     begin
       return yield
     rescue retry_exception
+      sleep delay unless delay.nil?
       retry if (retries -= 1) > 0
     end
 
--- lib/s3_store.rb
+++ lib/s3_store.rb
@@ -3,6 +3,9 @@ class S3VideoObject < AWS::S3::S3Object
 end
 
 class S3Store < AbstractStore
+
+  DELAY = 3
+
   def initialize
     raise Panda::ConfigError, "You must specify videos_domain and s3_videos_bucket to use s3 storage" unless Panda::Config[:videos_domain] && Panda::Config[:s3_videos_bucket]
     
@@ -20,10 +23,9 @@ class S3Store < AbstractStore
   # Set file. Returns true if success.
   def set(key, tmp_file)
     begin
-      retryable(:tries => 5) do
+      retryable(:tries => 5, :delay => DELAY) do
         Merb.logger.info "Upload to S3"
         S3VideoObject.store(key, File.open(tmp_file), :access => :public_read)
-        sleep 3
       end
     rescue AWS::S3::S3Exception
       Merb.logger.error "Error uploading #{key} to S3"
@@ -36,12 +38,11 @@ class S3Store < AbstractStore
   # Get file.
   def get(key, tmp_file)
     begin
-      retryable(:tries => 5) do
+      retryable(:tries => 5, :delay => DELAY) do
         File.open(tmp_file, 'w') do |file|
           Merb.logger.info "Fetch from S3"
           S3VideoObject.stream(key) {|chunk| file.write chunk}
         end
-        sleep 3
       end
     rescue AWS::S3::S3Exception
       raise_file_error(key)
@@ -53,10 +54,9 @@ class S3Store < AbstractStore
   # Delete file. Returns true if success.
   def delete(key)
     begin
-      retryable(:tries => 5) do
+      retryable(:tries => 5, :delay => DELAY) do
         Merb.logger.info "Deleting #{key} from S3"
         S3VideoObject.delete(key)
-        sleep 3
       end
     rescue AWS::S3::S3Exception
       raise_file_error(key)
