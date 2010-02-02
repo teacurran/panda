f5a0f001a0d3d8f045f8fa6c1799bb813aecf811
dcparker <dcparker@gmail.com>
2010-02-02 14:59:29 -0500

    config/error_messages.yml.example is now copied in deploy process.

--- config/deploy/support/setup.rb
+++ config/deploy/support/setup.rb
@@ -3,4 +3,5 @@ task :create_shared_config do
   run "mkdir -p #{shared_path}/config"
   put File.read("config/panda_init.rb.example"), "#{shared_path}/config/panda_init.rb"
   put File.read("config/mailer.rb.example"), "#{shared_path}/config/mailer.rb"
+  put File.read("config/error_messages.yml.example"), "#{shared_path}/config/error_messages.yml"
 end
