ae255d69d4aa783ce7825c96a6588b630ba3dbe4
Zach Dennis <zach.dennis@gmail.com>
2010-02-02 15:20:07 -0500

    Updated the production deploy configuration to point to the AWS EC2 instance.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 set(:user) { "prod" }
-set(:domain) { "209.20.71.35" }
+set(:domain) { 
+  # ec2-174-129-132-36.compute-1.amazonaws.com
+  "panda.iremix.org" 
+}
 ssh_options[:port] = 45001
 set :branch, "master"
 # For merb this will make little difference for this project...
