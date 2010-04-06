be77e869f80a63e5afeae7e1092d4fb9b3b87ce3
dcparker <dcparker@gmail.com>
2010-04-06 10:27:22 -0400

    Modified deploy script to use latest 'immortalize' conventions.

--- config/deploy/support/deploy.rb
+++ config/deploy/support/deploy.rb
@@ -3,8 +3,10 @@ $MERB = "/opt/ruby-enterprise-1.8.7-2009.10/bin/merb"
 
 namespace :deploy do
   desc "Immortalize Merb & services (cron job makes sure they keep running)"
-  task :immortalize_daemons, :roles => :app do
-    sudo "#{$IMMORTALIZE} --log-location /home/prod/.immortalize setup"
+  task :immortalize_setup, :roles => :app do
+    run "#{$IMMORTALIZE} setup"
+    run "#{$IMMORTALIZE} stop all"
+    run "#{$IMMORTALIZE} remove all"
   end
 
   desc "Start Merb & services"
@@ -30,4 +32,7 @@ namespace :deploy do
     run "#{$IMMORTALIZE} stop \"cd \\\"#{current_path}\\\"; #{$MERB} -e #{merb_env} -p 4002 -r bin/notifier.rb 1>>log/notifier.#{merb_env}.log 2>&1\""
     run "#{$IMMORTALIZE} --notify=rw-staging@mutuallyhuman.com run \"cd \\\"#{current_path}\\\"; #{$MERB} -e #{merb_env} -p 4002 -r bin/notifier.rb 1>>log/notifier.#{merb_env}.log 2>&1\""
   end
+
+  desc "Reload Merb & services"
+  task :reload => [:immortalize_setup, :start]
 end
