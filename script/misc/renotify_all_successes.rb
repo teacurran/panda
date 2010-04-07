1bfede52a540658c5686b199114ae67f6df0bde7
dcparker <dcparker@gmail.com>
2010-04-07 16:23:53 -0400

    Further updates to the renotify script.

--- script/misc/renotify_all_successes.rb
+++ script/misc/renotify_all_successes.rb
@@ -2,8 +2,12 @@
 cutoff_date = Time.parse('2010-04-01')
 Video.each do |video|
   if video.created_at > cutoff_date
-    puts "Renotifying for #{video.key} (#{video.created_at})"
-    Notification.add_video(video) rescue puts "!! No Parent for video ##{video.key}"
+    begin
+      Notification.add_video(video)
+      puts "Renotifying for #{video.key} (#{video.created_at})"
+    rescue
+      puts "!! No Parent for video ##{video.key}"
+    end
   else
     print "."
   end
