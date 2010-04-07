0e2b118556d2054457edb6f8617511e8fc94117f
dcparker <dcparker@gmail.com>
2010-04-07 16:21:47 -0400

    Lacking success with SDB queries to try to retrieve all videos that have been created after a specific date, I have switched to the method of querying for all videos and filtering in ruby.

--- script/misc/renotify_all_successes.rb
+++ script/misc/renotify_all_successes.rb
@@ -1,5 +1,10 @@
-Video.each_successful_encoding_since(Date.today - 1.day) do |video|
 # Video.each_successful_encoding do |video|
-  puts "Renotifying for #{video.inspect}"
-  Notification.add_video(video) rescue puts "!! No Parent for video ##{video.key}"
+cutoff_date = Time.parse('2010-04-01')
+Video.each do |video|
+  if video.created_at > cutoff_date
+    puts "Renotifying for #{video.key} (#{video.created_at})"
+    Notification.add_video(video) rescue puts "!! No Parent for video ##{video.key}"
+  else
+    print "."
+  end
 end
