6ee5e1c802a01ff36d42d599adcf964b460fee42
dcparker <dcparker@gmail.com>
2010-04-27 13:45:45 -0400

    Updated deploy-immortalize commands to work within the "panda" group space.

--- config/deploy/support/immortalize.rb
+++ config/deploy/support/immortalize.rb
@@ -2,22 +2,20 @@ namespace :immortalize do
   desc "Immortalize Merb & services (cron job makes sure they keep running)"
   task :setup, :roles => :app do
     run "#{immortalize_cmd} setup"
-    run "#{immortalize_cmd} stop all"
-    run "#{immortalize_cmd} remove all"
+    run "#{immortalize_cmd} stop --group panda"
+    run "#{immortalize_cmd} remove --group panda"
   end
 
   desc "Start Merb & services"
   task :start, :roles => :app do
-    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4000 1>>log/#{merb_env}.log 2>&1\""
-    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4001 -r bin/encoder.rb 1>>log/encoder.#{merb_env}.log 2>&1\""
-    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4002 -r bin/notifier.rb 1>>log/notifier.#{merb_env}.log 2>&1\""
+    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com --group panda run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4000 1>>log/#{merb_env}.log 2>&1\""
+    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com --group panda run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4001 -r bin/encoder.rb 1>>log/encoder.#{merb_env}.log 2>&1\""
+    run "#{immortalize_cmd} --notify=rw-staging@mutuallyhuman.com --group panda run \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4002 -r bin/notifier.rb 1>>log/notifier.#{merb_env}.log 2>&1\""
   end
   
   desc "Stop Merb & services"
   task :stop, :roles => :app do
-    run "#{immortalize_cmd} stop \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4000 1>>log/#{merb_env}.log 2>&1\""
-    run "#{immortalize_cmd} stop \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4001 -r bin/encoder.rb 1>>log/encoder.#{merb_env}.log 2>&1\""
-    run "#{immortalize_cmd} stop \"cd \\\"#{current_path}\\\"; #{merb_cmd} -e #{merb_env} -p 4002 -r bin/notifier.rb 1>>log/notifier.#{merb_env}.log 2>&1\""
+    run "#{immortalize_cmd} stop --group panda"
   end
 
   desc "Restart Merb & services"
